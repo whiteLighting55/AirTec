9a515ac69a5caa3ea88d67296ffa1bfdbf01bf78
Manu Franco <manuel_game555@hotmail.com>
2023-10-24 15:41:42 -0600

    .

--- Manu/drone.py
+++ Manu/drone.py
@@ -1,10 +1,6 @@
-# aquí va el code y mmas cosas
-#wenas
-
 import cv2
+import pygame
 from djitellopy import Tello
-import tkinter as tk
-from PIL import Image, ImageTk
 
 # Inicializar el dron Tello
 tello = Tello()
@@ -12,45 +8,42 @@ tello.connect()
 tello.streamoff()
 tello.streamon()
 
-# Crear una ventana de tkinter
-root = tk.Tk()
-root.title("Dron Tello Line Detection")
+# Inicializar pygame
+pygame.init()
+win = pygame.display.set_mode((960, 720))
+pygame.display.set_caption("Tello Line Detection")
 
-# Función para actualizar el fotograma
-def update_frame():
+running = True
+while running:
+    # Obtener el fotograma desde el dron Tello
     frame = tello.get_frame_read().frame
+
+    # Convertir el fotograma a escala de grises
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Aplicar detección de bordes para resaltar las líneas
     edges = cv2.Canny(gray_frame, 50, 150)
+
+    # Buscar líneas en el fotograma
     lines = cv2.HoughLinesP(edges, 1, cv2.CV_PI / 180, threshold=100, minLineLength=100, maxLineGap=50)
 
+    # Dibujar las líneas encontradas en el fotograma original
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    # Convertir el fotograma de OpenCV a formato de imagen de PIL
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(img)
-    img = ImageTk.PhotoImage(img)
-    panel.img = img
-    panel.config(image=img)
-    panel.after(10, update_frame)
+    # Convertir el fotograma de OpenCV a formato de superficie de pygame
+    frame_surface = pygame.surfarray.make_surface(cv2.flip(frame, 1))
+    win.blit(frame_surface, (0, 0))
+    pygame.display.update()
 
-# Crear un panel para mostrar la imagen en la ventana
-panel = tk.Label(root)
-panel.pack(padx=10, pady=10)
+    # Verificar eventos de pygame para cerrar la ventana
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-# Llamar a la función para actualizar el fotograma
-update_frame()
-
-# Función para cerrar la conexión del dron y salir del programa
-def on_closing():
-    tello.streamoff()
-    tello.disconnect()
-    root.destroy()
-
-# Configurar la función para cerrar la ventana
-root.protocol("WM_DELETE_WINDOW", on_closing)
-
-# Iniciar el bucle principal de la interfaz gráfica
-root.mainloop()
+# Liberar los recursos
+tello.streamoff()
+tello.disconnect()
+pygame.quit()
